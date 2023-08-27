1d2a2d077834a39047953a43e814276ddfc249f2
Ailtss <ailtongfd@gmail.com>
2023-08-27 11:22:24 -0300

    Adding done exercise from notebook

--- part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
+++ part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
@@ -1,4 +1,54 @@
 
-public class ExerciseManagement {
+import java.util.ArrayList;
 
+
+public class ExerciseManagement {
+    
+    private ArrayList<Exercise> exercises;
+    
+    public ExerciseManagement() {
+        
+        this.exercises = new ArrayList<Exercise>();
+    }
+    
+    public ArrayList<String> exerciseList(){
+        
+        ArrayList<String> list = new ArrayList<String>();
+        
+        for (Exercise e: this.exercises) {
+            
+            list.add(e.getName());
+        }
+        
+        return list;
+    }
+    
+    public void add(String exercise){
+        
+        this.exercises.add(new Exercise(exercise));
+    }
+    
+    public void markAsCompleted(String exercise) {
+        
+        for (Exercise e: this.exercises) {
+            
+            if (e.getName().equals(exercise)) {
+                
+                e.setCompleted(true);
+            }
+        }
+    }
+    
+    public boolean isCompleted(String exercise) {
+        
+        for (Exercise e: this.exercises) {
+            
+            if (e.getName().equals(exercise)) {
+                
+                return e.isCompleted();
+            }
+        }
+        
+        return false;
+    }
 }
--- part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,57 @@
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import org.junit.Before;
+import org.junit.Test;
+
+
 
 
 public class ExerciseManagementTest {
-
+    
+    private ExerciseManagement man;
+    
+    @Before
+    public void initialize() {
+        
+        man = new ExerciseManagement();
+    }
+    
+    @Test
+    public void exerciseListEmptyAtBeginning() {
+        
+        assertEquals(0, man.exerciseList().size());
+    }
+    
+    @Test
+    public void addingExerciseGrowsListByOne(){
+        
+        man.add("Write a test");
+        assertEquals(1, man.exerciseList().size());
+    }
+    
+    @Test
+    public void addedExerciseIsInList(){
+        
+        man.add("Write a test");
+        assertTrue(man.exerciseList().contains("Write a test"));
+    }
+    
+    @Test
+    public void exerciseCanBeMarkedAsCompleted() {
+        
+        man.add("New exercise");
+        man.markAsCompleted("New exercise");
+        assertTrue(man.isCompleted("New exercise"));
+        
+    }
+    
+    @Test
+    public void ifNotMarkedCompletedIsNotCompleted() {
+        
+        man.add("New exercise");
+        man.markAsCompleted("New exercise");
+        assertFalse(man.isCompleted("Some exercise"));
+    }
 }
