72313807d2d40db0feb57e82481a29d121d9f297
Ailton <ailtongfd@gmail.com>
2024-02-22 18:45:36 -0300

    Adding exercise

---
+++ part08-Part08_14.DictionaryOfManyTranslations/src/main/java/DictionaryOfManyTranslations.java
@@ -0,0 +1,43 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuário
+ */
+public class DictionaryOfManyTranslations {
+    
+    private HashMap<String, ArrayList<String>> dic;
+    
+    public DictionaryOfManyTranslations() {
+        
+        this.dic = new HashMap<>();
+    }
+    
+    public void add(String word, String translation) {
+        
+        this.dic.putIfAbsent(word, new ArrayList<>());
+        
+        this.dic.get(word).add(translation);
+    }
+    
+    public ArrayList<String> translate(String word) {
+        
+        return this.dic.getOrDefault(word, new ArrayList<>());
+    }
+    
+    public void remove(String word) {
+        
+        this.dic.remove(word);
+    }
+    
+    
+    
+}
